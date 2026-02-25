0df67e3028859038a83f8e21fe99069e5ab5f7f9
Felipe Dornelles <lipe.dornelles@hotmail.com>
2026-02-25 09:33:36 -0300

    feat: configure markdown-it plugins for footnotes and task lists

--- src/lib/markdownIt.ts
+++ src/lib/markdownIt.ts
@@ -1,7 +1,11 @@
 import markdownIt from "markdown-it";
+import markdownItTaskLists from "markdown-it-task-lists";
+import markdownItFootnote from "markdown-it-footnote";
 
 export const md = markdownIt({
-  html: true, // Permite tags HTML dentro do Markdown
-  linkify: true, // Transforma URLs em links automaticamente
-  breaks: true, // Transforma quebras de linha (enter) em <br>
-});
+  html: true,
+  linkify: true,
+  breaks: true,
+})
+  .use(markdownItTaskLists, { enabled: true, label: true })
+  .use(markdownItFootnote);
